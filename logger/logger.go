9c837465e9e59c3e85942a38ec1f3c1f44960ce5
tajud99n <giwa208@gmail.com>
2020-05-22 13:30:41 +0100

    logger util

---
+++ logger/logger.go
@@ -0,0 +1,98 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	envlogLevel  = "LOG_LEVEL"
+	envlogOutput = "LOG_OUTPUT"
+)
+
+var (
+	log logger
+)
+
+type bookstoreLogger interface {
+	Printf(format string, v ...interface{})
+}
+
+type logger struct {
+	log *zap.Logger
+}
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{getOutput()},
+		Level:       zap.NewAtomicLevelAt(getLevel()),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	var err error
+	if log.log, err = logConfig.Build(); err != nil {
+		panic(err)
+	}
+}
+
+func getLevel() zapcore.Level {
+	switch os.Getenv(envlogLevel) {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+func getOutput() string {
+	output := strings.TrimSpace(os.Getenv(envlogOutput))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
+func GetLogger() bookstoreLogger {
+	return log
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func (l logger) Print(v ...interface{}) {
+	Info(fmt.Sprintf("%v", v))
+}
+
+func Info(msg string, tags ...zap.Field) {
+	log.log.Info(msg, tags...)
+	log.log.Sync()
+}
+
+func Error(msg string, err error, tags ...zap.Field) {
+	if err != nil {
+		tags = append(tags, zap.NamedError("error", err))
+	}
+	log.log.Error(msg, tags...)
+	log.log.Sync()
+}
